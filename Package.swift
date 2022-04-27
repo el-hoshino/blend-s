c028286466d75d3a73bf5ed0459bc7fce1c9f8fb
Elvis Shi <3942121+el-hoshino@users.noreply.github.com>
2022-04-28 03:26:13 +0900

    Set target and product from library to executable

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "blend-s",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
+        .executable(
             name: "blend-s",
             targets: ["blend-s"]),
     ],
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .executableTarget(
             name: "blend-s",
             dependencies: []),
         .testTarget(

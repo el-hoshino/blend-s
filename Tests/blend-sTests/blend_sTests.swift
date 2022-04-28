70282933a836be690b235e84dac575308332d743
Elvis Shi <3942121+el-hoshino@users.noreply.github.com>
2022-04-28 15:29:14 +0900

    Comment out tests for now

--- Tests/blend-sTests/blend_sTests.swift
+++ Tests/blend-sTests/blend_sTests.swift
@@ -6,6 +6,7 @@ final class blend_sTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(blend_s().text, "Hello, World!")
+        // TODO: Tests
+//        XCTAssertEqual(blend_s().text, "Hello, World!")
     }
 }
